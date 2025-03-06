1fd03dd06fc5ccb188f1e8c14fe975ec7729d718
FAnoFOUR <antoniohaixu.tang@studenti.unipd.it>
2025-03-06 21:15:06 +0100

    VideoGames.h eand VideoGames.cpp changes. TODO on gameType and questions on delete

--- src/Model/lib/VideoGames.h
+++ src/Model/lib/VideoGames.h
@@ -3,27 +3,42 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QTextStream>
-#include "Video.h"
+#include "Multimedia.h"
 
-class Games : public Multimedia {
+class VideoGames : public Multimedia {
    private:
         string gameEngine;
-        enum gameType {FPS, RPG, RTS, MOBA, MMORPG, SIMULATION, SPORTS, PUZZLE, PLATFORMER, RACING, FIGHTING, SURVIVAL, ADVENTURE, ACTION, STRATEGY, 
-            SANDBOX, TOWERDEFENSE, CARDGAME, BOARDGAME, PARTYGAME, EDUCATIONAL, MUSIC, CASUAL, ARCADE, SIMULATOR, VIRTUALREALITY, OTHER};
+        // *TODO: se è possibile cambiarlo come Content?
+        // ? enum gameType {FPS, RPG, RTS, MOBA, MMORPG, SIMULATION, SPORTS, PUZZLE, PLATFORMER, RACING, FIGHTING, SURVIVAL, ADVENTURE, ACTION, STRATEGY, 
+        // ?  SANDBOX, TOWERDEFENSE, CARDGAME, BOARDGAME, PARTYGAME, EDUCATIONAL, MUSIC, CASUAL, ARCADE, SIMULATOR, VIRTUALREALITY, OTHER};
         unsigned int expectedHours;
     
     public:
-        Games(string _title, bool _subGenre[20], string _description, bool _starred, bool _watched, unsigned int _year, string _image, string _producer, 
+        VideoGames(string _title, bool _subGenre[20], string _description, bool _starred, bool _watched, unsigned int _year, string _image, string _producer, 
             string _language, string _gameEngine, unsigned int _expectedHours, QList <string> _platforms, Content* _inspiration=NULL);
         
         string getGameEngine();
-        string getGameType();
+        
+        // *TODO: da finire quando si conferma gameType
+        // ? string getGameType();
+
         unsigned int getExpectedHours();
         
         void setGameEngine(string gameEngine);
-        void setGameType(string gameType);
+
+        // *TODO: da finire quando si conferma gameType
+        // ? void setGameType(string gameType);
+
         void setExpectedHours(unsigned int expectedHours);
-        virtual ~Games();
+        virtual ~Games(){
+            // *TODO: connect(this, this.broadCastDelete(), tutti??, checkDeleteDependency())
+        };
+
+        
+        //*TODO: private slot: 
+        //? checkDeleteDependency(){ if( (*inspiration).isEqual(oggettoRicevuto) delete inspiration;){}  };
+        // Un idea a grande linee
+        
 };
 
 #endif
