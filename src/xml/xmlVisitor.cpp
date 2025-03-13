cb5deb5f7247a213543e3bcae09c4f6fc72477a7
Solgio <lorenzosoligo04@gmail.com>
2025-03-13 12:05:16 +0100

    xmlVisitor setup

--- src/xml/xmlVisitor.cpp
+++ src/xml/xmlVisitor.cpp
@@ -1,1 +1,17 @@
 #include "xmlVisitor.h"
+
+void xmlVisitor::toXml(Book *book){
+    //TODO
+};
+void xmlVisitor::toXml(Comic *comic){
+    //TODO
+};
+void xmlVisitor::toXml(Film *movie){
+    //TODO
+};
+void xmlVisitor::toXml(Series *series){
+    //TODO
+};
+void xmlVisitor::toXml(VideoGame *videogame){
+    //TODO
+};
