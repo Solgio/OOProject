949ef4a00918316501f7e7b5cc929dc8a15049c5
Solgio <lorenzosoligo04@gmail.com>
2025-03-24 12:02:03 +0100

    xml Modification

---
+++ src/xml/xmlReader.cpp
@@ -0,0 +1,26 @@
+#include <QtCore>
+#include <QtXml>
+#include <QDebug>
+
+QDomDocument document;
+
+int main() {
+    QFile file("SOMETHING.xml");
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        qDebug() << "Failed to open file for writing";
+        return -1;
+    }
+    else{
+        if(document.setContent(&file)){
+            qDebug() << "Failed to load document";
+            return -1;
+        }
+        file.close();
+    }
+
+    QDomElement root = document.firstChildElement();
+
+    qDebug() << "Finished";
+    return 0;
+}
+ 
