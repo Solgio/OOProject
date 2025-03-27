b04f1527d451495e8e24357563f0f8fda030432a
Solgio <lorenzosoligo04@gmail.com>
2025-03-27 13:07:02 +0100

    Reader interface

--- src/data_persistency/IReader.h
+++ src/data_persistency/IReader.h
@@ -1,2 +1,14 @@
-#include <QtCore>
+#ifndef IREADER_H
+#define IREADER_H
+#include <QtXml>
 #include <QJsonObject>
+#include "../Model/lib/Content.h"
+
+class IReader{
+    public:
+    virtual ~IReader() {};
+    virtual Content* read(const QJsonObject& object) = 0;
+    virtual Content* read(QDomElement& element) = 0;
+};
+
+#endif
