44107f8e8c4d487305a567ea82953e04b1710d02
FAnoFOUR <antoniohaixu.tang@studenti.unipd.it>
2025-04-30 12:54:38 +0200

    #13 quick adjust

--- src/View/ComicDisplayWindow.cpp
+++ src/View/ComicDisplayWindow.cpp
@@ -2,18 +2,18 @@
 
 ComicDisplayWindow::ComicDisplayWindow():
     BookDisplayWindow(),
-    Illustrator(""),
-    Serie(""),
-    Finished("")
+    Illustrator("", this),
+    Serie("", this),
+    Finished("", this)
 {
     this->format();
 }
 
 ComicDisplayWindow::ComicDisplayWindow(Comic *comic):
     BookDisplayWindow(comic),
-    Illustrator(QString::fromStdString(comic->getIllustrator())),
-    Serie(QString::fromStdString(comic->getSerie())),
-    Finished(comic->getFinished() ? "Yes" : "No")
+    Illustrator(QString::fromStdString(comic->getIllustrator()), this),
+    Serie(QString::fromStdString(comic->getSerie()), this),
+    Finished(comic->getFinished() ? "Yes" : "No", this)
 {
     this->format();
 }
