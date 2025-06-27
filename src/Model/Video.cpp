0c254d4344e0016b353d1d883b030adc2fc72334
FAnoFOUR <antoniohaixu.tang@studenti.unipd.it>
2025-06-27 15:18:22 +0200

    Adjusted setPrequel and setSequel in Video

--- src/Model/Video.cpp
+++ src/Model/Video.cpp
@@ -35,9 +35,11 @@ void Video::setPrequel(const unsigned int& _prequelId){
         }
         // Check if the prequel ID exists in the library and if it is a Video type Content
         auto tprequel = dynamic_cast<Video*>(ScienceFiction_Library::getInstance().searchId(_prequelId));
-        if(tprequel && tprequel->getSequel() != this->getId()){
+        if(tprequel){
             prequel = _prequelId;
-            tprequel->setSequel(this->getId());
+            if(tprequel->getSequel() != this->getId()){
+                tprequel->setSequel(this->getId());
+            }
         }
         else{
             qDebug() << "Prequel with this Id not found";
@@ -53,9 +55,11 @@ void Video::setSequel(const unsigned int& _sequelId){
         }
     //Check if the sequel ID exists in the library and if it is a Video type Content
         auto tsequel=dynamic_cast<Video*>(ScienceFiction_Library::getInstance().searchId(_sequelId));
-        if(tsequel && tsequel->getPrequel() != this->getId()){
+        if(tsequel){
             sequel = _sequelId;
-            tsequel->setPrequel(this->getId());
+            if(tsequel->getPrequel() != this->getId()){
+                tsequel->setPrequel(this->getId());
+            }
         }
         else{
             qDebug() << "Sequel with this Id not found";
